15ac2260d2f23d87daa803a1552f43f833ec5418
Malcolm <mbalchcr@oberlin.edu>
2013-08-02 20:27:44 -0400

    added posting of history to comments

--- history_bot.py
+++ history_bot.py
@@ -16,13 +16,39 @@ def get_comment_history(username):
     sort = sorted(subs, key=subs.get, reverse=True)
     for k in sort:
         print k+" "+str(float(subs[k] / float(total_posts)))    
+    return (sort, total_posts, subs)
+
+def post_comment(url, limit):
+    comment = _reddit.get_submission(url).comments[0]
+    author = comment.author
+    print comment
+    print author
+    hist = get_comment_history(author)
+    sublist = ''
+    count = 0
+    for k in hist[0]:
+        if count >= limit:
+            break 
+        sublist += '\n\n'+k+": "+str(float(hist[2][k]) / float(hist[1]) * 100)[:4]+"%"
+        count += 1
+
+    resp = "This from someone whose comment history is:"+sublist
+    comment.reply(resp)
+    print resp
+    
 
 def connect():
     global _reddit
     r = praw.Reddit(user_agent='com_history')
+    r.login('','')
     _reddit = r
     
 if __name__ == "__main__":
     connect()
-    uname = sys.argv[1]
-    get_comment_history(uname)
+    if sys.argv[1] == '-l':
+        uname = sys.argv[2]
+        get_comment_history(uname)
+    elif sys.argv[1] == '-p':
+        url = sys.argv[2]
+        limit = sys.argv[3]
+        post_comment(url,limit)
